b0fcc36f538f06172de0ae159a91034b1443839d
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2019-10-18 19:31:54 +0530

    Added Content

---
+++ Functions.py
@@ -0,0 +1,30 @@
+"""
+-Chapter 2-
+ \Functions/
+ 
+->It is systematic way of solving a problem by dividing the problem into several
+sub-problems and then integrating them and form final program.
+->This approach also called stepwise refinement method or modular approach.
+
+ =Built-in Functions=
+
+1. input function: accepts string from user without evaluating its value.
+   Example-
+"""
+name=input("Enter a name:")
+print("---------\n")
+"""
+2. eval function: evaluates the value of string.
+  Example-
+"""
+eval('15+10')
+print("---------\n")
+"""
+3. Composition function: value returned by fun may be used as an argument for another
+fun in a nested manner. Its called composition.
+"""
+n1=eval(input("Enter a number: "))
+n2=eval(input("Enter arithmetic expression: "))
+"""
+More Content coming
+"""
